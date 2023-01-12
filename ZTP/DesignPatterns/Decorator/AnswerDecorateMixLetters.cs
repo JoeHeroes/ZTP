4b10f764ac378139fa8de0f08cce00a6ded1ba2e
Eryk Hordejuk <erykh0@op.pl>
2023-01-12 22:35:19 +0100

    add decorator without testing

---
+++ ZTP/DesignPatterns/Decorator/AnswerDecorateMixLetters.cs
@@ -0,0 +1,47 @@
+﻿namespace ZTP.DesignPatterns.Decorator
+{
+    public class AnswerDecorateMixLetters : AnswersDecorator
+    {
+        public List<string> DecorateAnswers(List<string> words)
+        {
+            string correctWord = words.First();
+            List<string> answerList = new List<string>();
+            answerList.Add(correctWord);
+
+            for (int i = 0; i < 3; i++)
+            {
+                string mixWord = MixLetters(correctWord);
+                answerList.Add(mixWord);
+            }
+
+            return answerList;
+        }
+
+        public string MixLetters(string word)
+        {
+            int wordLength = word.Length;
+            char[] liters = new char[wordLength];
+            for (int i = 0; i < wordLength; i++)
+            {
+                liters[i] = word[i];
+            }
+
+            Random random = new Random();
+            int number = random.Next(word.Length) + 1;
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 1; j < wordLength - 1; j++)
+                {
+                    char temp = liters[j];
+                    liters[j] = liters[j + 1];
+                    liters[j + 1] = temp;
+                }
+            }
+
+            word = Convert.ToString(liters);
+
+            return word;
+        }
+    }
+}
---
+++ ZTP/DesignPatterns/Decorator/AnswerDecoratorMixList.cs
@@ -0,0 +1,24 @@
+﻿using ZTP.Models;
+
+namespace ZTP.DesignPatterns.Decorator
+{
+    public class AnswerDecoratorMixList : AnswersDecorator
+    {
+        public List<string> DecorateAnswers(List<string> words)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(words.Count);
+
+            for (int i = 0; i < randomNumber; i++)
+            {
+                string temp = words[0];
+                words[0] = words[1];
+                words[1] = words[2];
+                words[2] = words[3];
+                words[3] = temp;
+            }
+
+            return words;
+        }
+    }
+}
